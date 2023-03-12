9ca0d22a9f2c8c1082e122bbf1082c6dc3ead405
twill <twill1@gmx.com>
2023-03-11 23:35:06 -0600

    Variables

--- src/github.com/isoment/firstapp/Main.go
+++ src/github.com/isoment/firstapp/Main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+// We can declare variables in blocks
+var (
+	firstName  string = "Bill"
+	lastName   string = "Smith"
+	employeeId string = "ID4823"
+)
 
 func main() {
-	fmt.Println("Hello Go!")
+	fmt.Println("Variables")
+	var i int = 42
+	fmt.Printf("%v, %T\n", i, i)
+
+	// Variables can be converted from one type to another. We need to be careful with
+	// this since we can lose information during the conversion process. Ie going from
+	// a float to a number we can lose precision.
+	var j float32
+	j = float32(i)
+	fmt.Printf("%v, %T\n", j, j)
+
+	// When converting strings we can use the "strconv package" without this package Go
+	// will end up converting the int to it's corresponding unicode char.
+	var k string
+	k = strconv.Itoa(i)
+	fmt.Printf("%v, %T\n", k, k)
+
 }
