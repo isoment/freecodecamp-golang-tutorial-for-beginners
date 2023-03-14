375d6a0b65c807db635520a6fc8cb251921aba02
twill <twill1@gmx.com>
2023-03-13 21:58:30 -0500

    Arrays

--- src/github.com/isoment/firstapp/Main.go
+++ src/github.com/isoment/firstapp/Main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 /************
 * VARIABLES *
@@ -134,77 +132,118 @@ import (
 * Constants *
 ************/
 
-// iota is a counter we can use when creating enumerated constants. Iota is scoped to a constant
-// block so if we declare another constant block itoa will be reset to 0 for that block. It is a
-// good idea to use the first as an error in case we forget to declare a variable value later and
-// compare it to the iota
-const (
-	error = iota // 0
-	cat          // 1
-	dog          // 2
-	snake        // 3
-)
+// // iota is a counter we can use when creating enumerated constants. Iota is scoped to a constant
+// // block so if we declare another constant block itoa will be reset to 0 for that block. It is a
+// // good idea to use the first as an error in case we forget to declare a variable value later and
+// // compare it to the iota
+// const (
+// 	error = iota // 0
+// 	cat          // 1
+// 	dog          // 2
+// 	snake        // 3
+// )
 
-// Using a const block and iota to calculate how many bytes
-const (
-	_  = iota             // ignore first value by assigning to blank identifier
-	KB = 1 << (10 * iota) // 1024				iota is 1
-	MB                    // 1048576			iota is 2 etc...
-	GB                    // 1073741824
-	TB                    // 1099511627776
-)
+// // Using a const block and iota to calculate how many bytes
+// const (
+// 	_  = iota             // ignore first value by assigning to blank identifier
+// 	KB = 1 << (10 * iota) // 1024				iota is 1
+// 	MB                    // 1048576			iota is 2 etc...
+// 	GB                    // 1073741824
+// 	TB                    // 1099511627776
+// )
 
-/*
-Using const to set some role permission values. Each constant will represent one location in a byte,
-the first number is the decimal and the second the binary representation. We are just shifting to
-the left one binary place.
+// /*
+// Using const to set some role permission values. Each constant will represent one location in a byte,
+// the first number is the decimal and the second the binary representation. We are just shifting to
+// the left one binary place.
 
-We are able to use one byte to store 8 different permission roles. For example...
+// We are able to use one byte to store 8 different permission roles. For example...
 
-100101 has the roles isAdmin, canSeeFinancials and canSeeEurope
-*/
-const (
-	isAdmin            = 1 << iota // 1,    1
-	isHeadquarters                 // 2,    10
-	canSeeFinancials               // 4		100
-	canSeeAfrica                   // 8		1000
-	canSeeAsia                     // 16	10000
-	canSeeEurope                   // 32	100000
-	canSeeNorthAmerica             // 64	1000000
-	canSeeSouthAmerica             // 128	10000000
-)
+// 100101 has the roles isAdmin, canSeeFinancials and canSeeEurope
+// */
+// const (
+// 	isAdmin            = 1 << iota // 1,    1
+// 	isHeadquarters                 // 2,    10
+// 	canSeeFinancials               // 4		100
+// 	canSeeAfrica                   // 8		1000
+// 	canSeeAsia                     // 16	10000
+// 	canSeeEurope                   // 32	100000
+// 	canSeeNorthAmerica             // 64	1000000
+// 	canSeeSouthAmerica             // 128	10000000
+// )
+
+// func main() {
+// 	fmt.Println("Constants")
+// 	const myConst float64 = 1.5453454
+// 	fmt.Printf("%v, %T\n", myConst, myConst)
+
+// 	// Notice in this case how this is allowed. Even though the const is inferred to be of int
+// 	// type it is still allowed to be added to an int16 type. This works because the compiler
+// 	// is replacing const a with a literal value of the constant.
+// 	const a = 10
+// 	var b int16 = 2
+// 	fmt.Printf("%v, %T\n", a+b, a+b)
+
+// 	var animal int = dog
+// 	fmt.Printf("%v\n", animal == dog)
+
+// 	// Calculating filesize
+// 	fileSize := 4000000000
+// 	sizeGB := float32(fileSize) / GB
+// 	fmt.Printf("%.2fGB\n", sizeGB)
+
+// 	// Using the roles const from above, when we or the above values together we get a
+// 	var roles byte = isAdmin | canSeeFinancials | canSeeEurope
+// 	fmt.Printf("%b\n", roles)
+// 	/*
+// 		000001 isAdmin
+// 		100101 roles
+// 		------
+// 		000001
+
+// 		The expression below evaluates to true
+// 	*/
+// 	fmt.Printf("Is Admin? %v\n", isAdmin&roles == isAdmin)
+// }
+
+/*********
+* Arrays *
+*********/
 
 func main() {
-	fmt.Println("Constants")
-	const myConst float64 = 1.5453454
-	fmt.Printf("%v, %T\n", myConst, myConst)
+	fmt.Println("Arrays")
 
-	// Notice in this case how this is allowed. Even though the const is inferred to be of int
-	// type it is still allowed to be added to an int16 type. This works because the compiler
-	// is replacing const a with a literal value of the constant.
-	const a = 10
-	var b int16 = 2
-	fmt.Printf("%v, %T\n", a+b, a+b)
+	// Declaring the array and preassigning it values
+	grades := [3]int{90, 100, 85}
+	fmt.Printf("Grades: %v\n", grades)
 
-	var animal int = dog
-	fmt.Printf("%v\n", animal == dog)
+	// Declaring the array and assigning values after the fact
+	var students [3]string
+	students[0] = "Lisa"
+	students[1] = "Bill"
+	students[2] = "Frank"
+	fmt.Printf("Students: %v\n", students)
+	fmt.Printf("Student Count: %v\n", len(students))
 
-	// Calculating filesize
-	fileSize := 4000000000
-	sizeGB := float32(fileSize) / GB
-	fmt.Printf("%.2fGB\n", sizeGB)
+	// Arrays can be made up of arrays, ie a matrix
+	var identityMatrix [3][3]int
+	identityMatrix[0] = [3]int{1, 0, 0}
+	identityMatrix[1] = [3]int{0, 1, 0}
+	identityMatrix[2] = [3]int{0, 0, 1}
+	fmt.Printf("Identity matrix: %v\n", identityMatrix)
 
-	// Using the roles const from above, when we or the above values together we get a
-	var roles byte = isAdmin | canSeeFinancials | canSeeEurope
-	fmt.Printf("%b\n", roles)
-	/*
-		000001 isAdmin
-		100101 roles
-		------
-		000001
-
-		The expression below evaluates to true
-	*/
-	fmt.Printf("Is Admin? %v\n", isAdmin&roles == isAdmin)
-
+	a := [...]int{1, 2, 3}
+	b := a
+	b[1] = 5
+	fmt.Println(a)
+	// We can see that b is a new copy of array a and it is not simply pointing to it in memory.
+	fmt.Println(b)
+	// We can use a pointer, which is just a reference to the original value in memory. Here we set
+	// the value of c to point to a
+	c := &a
+	fmt.Println(c)
+	// When we change an element in c it is reflected in a since c is just a pointer to a
+	c[1] = 16
+	fmt.Println(c)
+	fmt.Println(a)
 }
